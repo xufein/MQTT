077fa0a65eb7496f327282adade06e3a396e5dd5
Xufei <xufein@kth.se>
2017-03-20 16:09:45 +0100

    new

--- src/main/java/client/SimpleMqttCallBack.java
+++ src/main/java/client/SimpleMqttCallBack.java
@@ -4,14 +4,21 @@ import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
-public class SimpleMqttCallBack implements MqttCallback {
+public class SimpleMqttCallBack extends client implements MqttCallback {
 	
+
 	public void connectionLost(Throwable throwable) {
 	    System.out.println("Connection lost");
 	}
 
-	public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-	    System.out.println("Message received: "+ new String(mqttMessage.getPayload()) );
+	public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+		String message = new String(mqttMessage.getPayload());
+		System.out.println("Message received: "+ message);
+		if (message.equalsIgnoreCase("test")) {
+			System.out.println("match");
+		}
+
+		System.out.println(super.s);
 	}
 
 	public void deliveryComplete(IMqttDeliveryToken token) {
--- src/main/java/client/client.java
+++ src/main/java/client/client.java
@@ -5,13 +5,22 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class client {
 	
+	public static String s;
+	
+	public static void setS(){
+		s = "test";
+	}
+	
 	public static void main(String[] args) throws MqttException {
 		String client = "tcp://localhost:1883";
-	    MqttClient mqttClient = new MqttClient(client, MqttClient.generateClientId());
-	    System.out.println("Start to subscribe: " + client);
-	    mqttClient.setCallback( new SimpleMqttCallBack() );
-	    mqttClient.connect();
-	    mqttClient.subscribe("TRS");
+	    MqttClient mqttClient;
+		mqttClient = new MqttClient(client, MqttClient.generateClientId());	    
+		System.out.println("Start to subscribe: " + client);
+		mqttClient.setCallback( new SimpleMqttCallBack() );
+		mqttClient.connect();
+		mqttClient.subscribe("TRS");
+		setS();
 	}
+
 	
 }
